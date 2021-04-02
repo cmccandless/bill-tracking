cb55c16c88287bf6468a7a8c4c07c60e2a0afa30
Corey McCandless <corey.mccandless@atlasied.com>
2021-04-02 15:04:24 -0400

    general improvements/fixes

--- bill_tracker.py
+++ bill_tracker.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 from datetime import date, datetime, timedelta
 from math import floor
 from pathlib import Path
-from typing import List
+from typing import List, Dict
 import yaml
 
 from tabulate import tabulate
@@ -23,12 +23,6 @@ class BillingCycle(str, Enum):
     Yearly = "yearly"
     Weekly = "weekly"
 
-    @property
-    def default_day(self) -> int:
-        if self.value == "weekly":
-            return 0
-        return 1
-
 
 @dataclass
 class Bill:
@@ -42,11 +36,11 @@ class Bill:
 
     notes: str = ""
 
+    paid_next: bool = False
+
     def __post_init__(self):
         if isinstance(self.cycle, str):
             self.cycle = BillingCycle(self.cycle)
-        if self.day == -1:
-            self.day = self.cycle.default_day
 
     def per_paycheck_estimate(self, pay_period):
         if self.cycle == BillingCycle.Monthly:
@@ -74,6 +68,10 @@ class Bill:
         due_date = start_date
         while not self.is_due(due_date):
             due_date += timedelta(days=1)
+        if self.paid_next:
+            due_date += timedelta(days=1)
+            while not self.is_due(due_date):
+                due_date += timedelta(days=1)
         return due_date
 
     def last_due_date(self, start_date: date) -> date:
@@ -82,37 +80,44 @@ class Bill:
             due_date -= timedelta(days=1)
         return due_date
 
+    def pay(self):
+        self.paid_next = True
+
     def needed_balance(
         self, on_day: date, last_pay_day: date, pay_period=timedelta(days=14)
     ) -> float:
         last_payment = self.last_due_date(on_day)
         next_payment = self.next_due_date(on_day)
         next_pay_day = last_pay_day + pay_period
+        needed = 0
         if self.cycle == BillingCycle.Monthly:
             if last_payment > last_pay_day:
-                return 0
+                needed = 0
             elif (next_payment - last_pay_day) < pay_period:
                 # due this pay period
-                return self.amount
+                needed = self.amount
             else:
-                return round(self.amount / 2, 2)
+                needed = round(self.amount / 2, 2)
         elif self.cycle == BillingCycle.Weekly:
             # one_week = timedelta(days=7)
             if next_payment > next_pay_day:
-                return 0
+                needed = 0
             elif last_payment < last_pay_day:
-                return self.amount * 2
+                needed = self.amount * 2
             return self.amount
         elif self.cycle == BillingCycle.Yearly:
             if last_payment > last_pay_day:
-                return 0
-            amount_due = 0
-            day = last_pay_day
-            while day > last_payment:
-                amount_due += self.amount / 26
-                day -= pay_period
-            return round(amount_due, 2)
-        return 0
+                needed = 0
+            else:
+                amount_due = 0
+                day = last_pay_day
+                while day > last_payment:
+                    amount_due += self.amount / 26
+                    day -= pay_period
+                needed = round(amount_due, 2)
+        if self.paid_next and needed >= self.amount:
+            needed -= self.amount
+        return needed
 
 
 @dataclass
@@ -121,6 +126,7 @@ class Budget:
     directdeposit: List[Bill] = None
     pay_period_days: int = 14
     minimum_balance: float = 0
+    paid: List[str] = None
 
     def __post_init__(self):
         for i, bill in enumerate(self.bills):
@@ -128,6 +134,12 @@ class Budget:
                 self.bills[i] = Bill(**bill)
         if self.directdeposit is None:
             self.directdeposit = []
+        if self.paid is None:
+            self.paid = {}
+        else:
+            for bill in self.bills:
+                if bill.name in self.paid:
+                    bill.pay()
         for i, bill in enumerate(self.directdeposit):
             if isinstance(bill, dict):
                 self.directdeposit[i] = Bill(**bill)
@@ -137,9 +149,9 @@ class Budget:
         return timedelta(days=self.pay_period_days)
 
     @classmethod
-    def load_from_file(cls, budget_file: Path) -> "Budget":
+    def load_from_file(cls, budget_file: Path, paid: List[str]) -> "Budget":
         with budget_file.open() as f:
-            return cls(**yaml.load(f, Loader=yaml.SafeLoader))
+            return cls(paid=paid, **yaml.load(f, Loader=yaml.SafeLoader))
 
     def estimate_paycheck(self, bills_only=False):
         bill_total = sum(
@@ -161,38 +173,29 @@ def hrule(length):
 
 
 def print_bill_allocations(bills: List[Bill], allocations: List[float]):
-
     today = date.today()
-    # yesterday = today - timedelta(days=1)
-    border = "|" + hrule(91) + "|"
-    print(border)
-    print(
-        f'| {"Bill":<41} | Allocated | Total   | {"Last Paid":<10} | '
-        f'{"Next Due":<10} |'
-    )
-    border_sep = "|".join(
-        ["", hrule(43), hrule(11), hrule(9), hrule(12), hrule(12), ""]
-    )
-    print(border_sep)
-    bill_info = (
-        (b.name, a, b.amount, b.last_due_date(today), b.next_due_date(today))
-        for b, a in zip(bills, allocations)
-    )
-    bill_info = sorted(bill_info, key=lambda i: i[3], reverse=True)
-    for name, allocated, amount, last_due, next_due in bill_info:
-        print(
-            f"| {name:<41} |   {allocated: 7.2f} | "
-            f"{amount:7.2f} | {last_due} | {next_due} |"
+    tomorrow = today + timedelta(days=1)
+    headers = ["Bill", "Allocated", "Total", "Cycle", "Last Paid", "Next Due"]
+    data = [
+        (
+            bill.name,
+            allocated,
+            bill.amount,
+            bill.cycle.name,
+            bill.last_due_date(today),
+            bill.next_due_date(tomorrow),
         )
-    print(border)
+        for bill, allocated in zip(bills, allocations)
+    ]
+    data = sorted(data, key=lambda d: d[5])
+    print(tabulate(data, headers=headers, tablefmt="github", floatfmt=".2f"))
 
 
 def get_allocations(budget: Budget, last_pay_day: date) -> List[float]:
     today = date.today()
+    tomorrow = today + timedelta(days=1)
     # yesterday = today - timedelta(days=1)
-    return [
-        bill.needed_balance(today, last_pay_day) for bill in budget.bills
-    ]
+    return [bill.needed_balance(tomorrow, last_pay_day) for bill in budget.bills]
 
 
 def find_current_margin(
